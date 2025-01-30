04b7898f67652269e724574153d17cff86579333
Volha Patsiahevich <zaprudskaya@gmail.com>
2025-01-30 13:38:50 +0100

    feat: implement quick game for lovers

--- you-and-me/src/App.tsx
+++ you-and-me/src/App.tsx
@@ -1,7 +1,42 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
+import Modal from './components/Modal/Modal';
+import Card from './components/Card/Card';
+import questions from './database/questions';
+
+const index = 0;
 
 function App() {
-  return <p>Hello world</p>;
+  const [questionIndex, setIndex] = useState(() => {
+    const savedIndex = localStorage.getItem('gameIndex');
+    return savedIndex ? Number(savedIndex) : index;
+  });
+
+  const [isStart, setIsStart] = useState(Boolean(questionIndex));
+
+  useEffect(() => {
+    localStorage.setItem('gameIndex', questionIndex.toString());
+  }, [questionIndex]);
+
+  function increaseIndex() {
+    setIndex((prevIndex) => prevIndex + 1);
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      {!isStart ? (
+        <Modal startGame={setIsStart} />
+      ) : (
+        <Card
+          number={questionIndex + 1}
+          text={questions[questionIndex]}
+          increaseIndex={increaseIndex}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
